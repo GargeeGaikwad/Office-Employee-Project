cef63716b0f778c41ab63e7929fca853a5c8aa29
GargeeGaikwad <gaikwadgargee@gmail.com>
2024-07-14 23:26:39 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,5 +1,4 @@
-#project urls
-"""office_emp_proj URL Configuration
+"""office_emp_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -16,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('emp_app.urls'))
+    path('',views.index,name='index'),
+    path('all_emp', views.all_emp, name='all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('remove_emp',views.remove_emp,name='remove_emp'),
+    path('remove_emp/<int:emp_id>', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp'),
 ]
+
